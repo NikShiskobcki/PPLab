c02d0340c55a1b90ec07369966135634d6553089
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-05-08 19:32:03 -0300

    Create sorteo.cpp

---
+++ sorteo.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+int generarCopa(int i){// es una funcion que genera un numero del 1 al 3.
+	return (i%3)+1;
+}
+	
+void estafa(int &saldoFinal, int cantApuesta, int copa){
+	saldoFinal -= cantApuesta;
+	switch(copa){
+	case 1: 
+		printf("  O O\n");
+		break;
+	case 2: 
+		printf("O   O\n");
+		break;
+	case 3:
+		printf("O O  \n");
+		break;
+	};
+	printf("No ha acertado\n");
+	printf("Saldo: %d", saldoFinal);
+}
+void gano(int &saldoFinal, int cantApuesta, int copa){
+	saldoFinal += cantApuesta;
+	switch(copa){
+		case 1: 
+			printf(". O O\n");
+			break;
+		case 2: 
+			printf("O . O\n");
+			break;
+		case 3:
+				printf("O O .\n");
+				break;
+	};
+	printf("Ha acertado\n");
+	printf("Saldo: %d", saldoFinal);
+}
+void perdio(int &saldoFinal, int cantApuesta, int copa){
+	saldoFinal -= cantApuesta;
+		switch(copa){
+	case 1: 
+		printf(". O O\n");
+		break;
+	case 2: 
+		printf("O . O\n");
+		break;
+	case 3:
+		printf("O O .\n");
+		break;
+	};
+	printf("No ha acertado\n");
+	printf("Saldo: %d", saldoFinal);
+}
+	
+int apuesta(int saldoFinal){
+	int cantidad;//cantidad que apuesta el usuario.
+	
+	do{
+		scanf("%d", &cantidad);
+	} while(cantidad < 0 || cantidad > saldoFinal);
+	return cantidad;
+}
