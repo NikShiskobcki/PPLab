d0962d0bf9e394f7ef56de75ccf31c50d0098d87
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-05-08 19:31:37 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include "sorteo.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int saldoInicial = 1000;
+	int ronda = 0;
+	int rGanadasSeguidas = 0; // RONDAS GANADAS SEGUIDAS.
+	int saldoFinal = saldoInicial;
+	int posElegida;// posicion elegida de la copa del usuario
+	int cantApuesta; // cantidad que quiera guardar/apostar el usuario.
+	int copa;
+	
+	while (saldoFinal > 0){
+		printf("\nO O O\n1 2 3\n");
+		printf("Copa?: ");
+		scanf("%d",&posElegida);
+		printf("Apuesta?: ");
+		cantApuesta = apuesta(saldoFinal);
+		ronda++;
+		
+		copa = generarCopa(ronda); 
+		
+		if(copa == posElegida){
+			if(rGanadasSeguidas == 2){
+				estafa(saldoFinal, cantApuesta, copa);
+				rGanadasSeguidas = 0;
+			}else{
+				gano(saldoFinal, cantApuesta, copa);
+				rGanadasSeguidas++;
+			}
+		}else{
+			perdio(saldoFinal, cantApuesta, copa);
+			rGanadasSeguidas = 0;
+		}
+	}
+	
+	return 0;
+}
